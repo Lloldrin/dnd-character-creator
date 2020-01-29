a6606aace337812d810f460a9ab79e76e7e09330
Douglas Victor Cedrum <douglsa.viced@gmail.com>
2020-01-29 23:59:49 +0100

    Fixed onclick functions to sort data

--- assets/js/external_api.js
+++ assets/js/external_api.js
@@ -1,8 +1,11 @@
-const dnd_api_url = `http://www.dnd5eapi.co/api/`;
+//Api Url
+const dnd_api = `http://www.dnd5eapi.co/api/`;
+
+/* ------------ Ingame Races ---------- */
 
 //Fetches  data to populate the race list 
 async function fetchRaceList() {
-    let availableRaces = await fetch(dnd_api_url + `races/`);
+    let availableRaces = await fetch(dnd_api + `races/`);
     let playerRace = await availableRaces.json();
     populateRaceList(playerRace.results);
     console.log(playerRace.results);
@@ -10,27 +13,25 @@ async function fetchRaceList() {
 
 //Listens for the user to make a race selection and displays the correct data
 $(`body`).on(`click`, `.btn_race`, async function () {
-    let clickedRace = await fetch(dnd_api_url + `races/` + $(this).attr(`id`) + `/`)
+    let clickedRace = await fetch(dnd_api + `races/` + $(this).attr(`id`))
     let currentRace = await clickedRace.json();
+    currentRaceInformation(currentRace);
     console.log(currentRace);
 })
 
-//Fetch race information based upon the click event
-async function fetchRaceInformation() {
-    let 
-}
+/* ------------ Ingame Classes ---------- */
 
 //Fetches  data to populate the class list 
 async function fetchClassList() {
-    let availableClasses = await fetch(dnd_api_url + `classes/`)
+    let availableClasses = await fetch(dnd_api + `classes/`)
     let playerClass = await availableClasses.json()
     populateClassList(playerClass.results)
     console.log(playerClass.results)
 }
 
-//Listens for the user to make a class selection and displays the correct data
+//Listens for the user to make a (game)class  selection and displays the correct data
 $(`body`).on(`click`, `.btn_class`, async function () { 
-    let clickedClass = await fetch(dnd_api_url + `classes/` + $(this).attr(`id`) + `/`)
+    let clickedClass = await fetch(dnd_api + `classes/` + $(this).attr(`id`))
     let currentClass = await clickedClass.json();
     console.log(currentClass);
 })
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,24 @@
-async function populateRaceList(raceList) {
-    raceList.forEach(element => {
+//Function to dynamically create the list of races and give each button a unique ID
+function populateRaceList(listRaces) {
+    listRaces.forEach(element => {
         $(`#race_list`).append(`<li><button type="button" class="btn btn_race btn-primary" id="${element.index}">${element.name}</button></li>`)
     })
 }
 
 fetchRaceList()
 
-async function populateClassList(classList) {
-    classList.forEach(element => {
+//Function to use the data retrieved on click of btn_race (prototype just to make sure that the data can be sent to the DOM) 
+function currentRaceInformation(currentRace) {
+    $(`#race_alignment`).text(`Alignment: ${currentRace.alignment}`);
+    $(`#race_age`).text(`Age: ${currentRace.age}`);
+    $(`#race_size_description`).text(`Size:${currentRace.size_description}`);
+    $(`#race_speed`).text(`Speed: ${currentRace.speed}`);
+
+}
+
+//Function to dynamically create the list of classes and give each button a unique ID
+function populateClassList(listClasses) {
+    listClasses.forEach(element => {
         $(`#class_list`).append(`<li><button type="button" class="btn btn_class btn-primary" id="${element.index}">${element.name}</button></li>`)
     });
 }
